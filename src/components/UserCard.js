9dee4de9abab1c05ddfced6c326dfebfca98a248
HASAN <alhasanal_qaysi@yahoo.com>
2021-11-24 21:37:01 +0200

    Added button to UserCard to navigate to UserDetails

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const UserCard = ({ name }) => {
+    const navigate = useNavigate();
+
     return (
-        <div>User card for {name}</div>
+        <div>
+            <h3>User card for {name}</h3>
+            <button onClick={() => navigate(name)}>get details</button>
+        </div>
     )
 }
 
