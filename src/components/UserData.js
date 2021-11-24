ae3c038f8030ad7cf50979e5c2385fe829ed2ceb
HASAN <alhasanal_qaysi@yahoo.com>
2021-11-25 00:44:21 +0200

    UserData

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,7 +1,26 @@
-const UserData = () => {
-    return (
-        <div>More data about user</div>
-    )
-}
+import { Row, Col } from "react-bootstrap";
 
+const UserData = ({ user }) => {
+    return (
+        <Row>
+            <Col>
+                <ul>
+                    <li>name: {user.name}</li>
+                    <li>username: {user.username}</li>
+                    <li>email: {user.email}</li>
+                    <li>phone: {user.phone}</li>
+                    <li>address:
+                        <ul>
+                            <li>street: {user.address.street}</li>
+                            <li>street: {user.address.suite}</li>
+                            <li>street: {user.address.city}</li>
+                            <li>street: {user.address.zipcode}</li>
+                        </ul>
+                    </li>
+                </ul>
+            </Col>
+        </Row>
+    )
+} 
+        
 export default UserData;
