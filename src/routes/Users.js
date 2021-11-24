05658506bd6a6e724fc44b8362413ed7bc2ebe41
HASAN <alhasanal_qaysi@yahoo.com>
2021-11-24 23:25:37 +0200

    Fatch users

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,15 +1,20 @@
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import UserCard from "../components/UserCard";
 
 const Users = () => {
-    const users = ["Hasan", "Tatiana", "Alisher"];
+    const [users, setUsers] = useState([]);
+    useEffect(() => fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => setUsers(users))
+        .catch(err => console.error(err)), []);
 
     return (
-        <Row>
+        <Row xs={1} md={2} lg={3} className="g-4 mt-4 mb-5">
             {users.map(user => {
                 return (
-                    <Col key={user}>
-                        <UserCard name={user} />
+                    <Col key={user.name}>
+                        <UserCard name={user.name} />
                     </Col>
                 )
             })}
